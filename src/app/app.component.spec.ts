909e77a89b695e6047b70f3b025d8acb838d4deb
Bhagya Lahiru <blkgamage@gmail.com>
2020-07-28 01:56:39 +0530

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Techwire'`, () => {
+  it(`should have as title 'firstProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Techwire');
+    expect(app.title).toEqual('firstProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Techwire app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('firstProject app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Techwire';
+
+
+
+constructor(){}
+
+
+
 }
